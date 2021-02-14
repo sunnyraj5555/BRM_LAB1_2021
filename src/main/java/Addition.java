49fc3f914e6a28981b0d9bf9e45016b51b01cc4e
sunnyraj5555 <headheal2206@gmail.com>
2021-02-14 18:54:07 +0530

    Commiting BRM lab1

---
+++ src/main/java/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition 
+{
+	public static void add(int a, int b)
+	{
+		int c= a+b;
+		System.out.println("sum : " + c);
+	}
+	
+	public static void main(String args[])
+	{
+		add(10,20);
+	}
+}
